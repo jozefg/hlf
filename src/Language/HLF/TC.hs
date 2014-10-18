b18087b2c1c67a6068f900513281a765446bc5ec
Danny Gratzer <jozefg@cmu.edu>
2014-10-18 15:14:02 -0400

    Added some higher level functions to TC

--- src/Language/HLF/TC.hs
+++ src/Language/HLF/TC.hs
@@ -1,22 +1,24 @@
 module Language.HLF.TC where
 import           Control.Monad
 import qualified Data.Map          as M
+import           Data.Maybe
 import           Language.HLF.AST
 import           Language.HLF.Eval
+
 
 type Context = M.Map Name Type
 type FreshCounter = Int
 
 typeInf :: FreshCounter -> Context -> InfTerm -> Maybe Type
 typeInf i cxt = go
-  where go (Ann un ty) = do
+  where go Star = Just VStar
+        go (Var j) = M.lookup (Bound j) cxt
+        go (Par n) = M.lookup n cxt
+        go (Ann un ty) = do
           let v = evalUnInf ty []
           typeUnInf i cxt ty VStar
           typeUnInf i cxt un v
           return v
-        go Star = Just VStar
-        go (Var j) = M.lookup (Bound j) cxt
-        go (Par n) = M.lookup n cxt
         go (f :@: a) =
           case typeInf i cxt f of
            Just (VPi ty retTy) ->
@@ -30,13 +32,18 @@ typeInf i cxt = go
           typeUnInf (i + 1) cxt' body' VStar
           return VStar
 
-
 typeUnInf :: FreshCounter -> Context -> UnInfTerm -> Type -> Maybe ()
 typeUnInf i cxt (Lam unInf) (VPi ty retTy) =
   let cxt' = M.insert (Bound i) ty cxt
       unInf' = substUnInf i (Par $ Bound i) unInf
-  in typeUnInf i cxt' unInf' (retTy . VNeutral . NPar . Bound $ i)
+  in typeUnInf (i + 1) cxt' unInf' (retTy . VNeutral . NPar . Bound $ i)
 typeUnInf i cxt (Inf term) t = do
   t' <- typeInf i cxt term
   guard (quote t' == quote t)
 typeUnInf _ _ _ _ = Nothing
+
+hasType :: UnInfTerm -> Type -> Bool
+hasType term ty = isJust $ typeUnInf 0 M.empty term ty
+
+typeOf :: InfTerm -> Maybe Type
+typeOf = typeInf 0 M.empty
