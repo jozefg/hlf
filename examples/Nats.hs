8c88d44ac78ca8276f3ff33a968b47d74d366357
Danny Gratzer <jozefg@cmu.edu>
2014-10-19 16:08:43 -0400

    Added example of natural numbers + addition

---
+++ examples/Nats.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Nats where
+import Language.HLF
+
+program :: Program
+program = Env [ "Nat" := Star
+              , "S"   := piTy "Nat" "" "Nat"
+              , "Z"   := "Nat"
+              , "plus" :=
+                piMany [("Nat", ""), ("Nat", ""), ("Nat", "")] Star
+              , "plusZ" := piTy "Nat" "N" ("plus" :@: "Z" :@: "N" :@: "N")
+              , "plusS" :=
+                piMany [("Nat", "N"), ("Nat", "M"), ("Nat", "O")]
+                (piTy ("plus":@:"N":@:"M":@:"O") ""
+                 ("plus":@:("S":@:"N"):@:"M":@:("S":@:"O")))]
