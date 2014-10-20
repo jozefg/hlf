2f1d6c6ca2fd8eee4ebe331edaf2f3b1f5ae6e3b
Danny Gratzer <danny.gratzer@gmail.com>
2014-10-20 12:44:00 -0400

    Typechecker now generates errors

--- src/Language/HLF/TC.hs
+++ src/Language/HLF/TC.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
 module Language.HLF.TC where
 import           Bound
-import           Control.Monad (guard)
-import qualified Data.Foldable     as T
-import qualified Data.Map          as M
+import           Control.Applicative
+import           Control.Lens         hiding (Context)
+import           Control.Monad        (guard)
+import           Control.Monad.Reader
+import qualified Data.Foldable        as F
+import qualified Data.Map             as M
 import           Data.Maybe
+import           Data.Monoid
+import qualified Data.Text            as Te
+import qualified Data.Traversable     as T
 import           Language.HLF.AST
+import           Language.HLF.Error
 import           Language.HLF.Eval
 
-assert :: [Maybe a] -> b -> Maybe b
-assert as b = T.sequence_ as >> return b
+type NameMap = M.Map Fresh Name
+type TyM = ErrorM (Reader NameMap)
+
+assert :: [TyM a] -> b -> TyM b
+assert as b = F.sequence_ as >> return b
 
 unBind :: Int -> Scope () Term Fresh -> Term Fresh
 unBind i sc = instantiate1 (Var (Unbound i)) sc
@@ -16,32 +27,52 @@ unBind i sc = instantiate1 (Var (Unbound i)) sc
 bind :: Int -> Term Fresh -> Context -> Context
 bind = M.insert . Unbound
 
-typeTerm :: Int -> Context -> Term Fresh -> Maybe (Term Fresh)
-typeTerm i cxt t = case t of
-  Star -> Just Star
-  Var j -> M.lookup j cxt
-  Lam body argTy -> typeTerm (i + 1) (bind i argTy cxt) $ unBind i body
-  f :@: a ->
-    case typeTerm i cxt f of
-     Just (Pi ty retTy) ->
-       assert [checkTerm i cxt a $ nf ty] (instantiate1 (nf a) retTy)
-     Just (When r l) ->
-       assert [checkTerm i cxt a $ nf l] (nf r)
-     _ -> Nothing
-  Pi ty body ->
-    assert [ isType i cxt ty
-           , isType (i + 1) (bind i (nf ty) cxt) (unBind i body)]
-    Star
-  When r l -> assert [isType i cxt $ nf l, isType i cxt $ nf r]
-              Star
+lookupVar :: Fresh -> Context -> TyM (Term Fresh)
+lookupVar i cxt = do
+  name <- lift $ asks (M.! i)
+  case M.lookup i cxt of
+   Just ty -> return ty
+   Nothing -> hlfError . Impossible $ "Found unbound name " <> name
 
-checkTerm :: Int -> Context -> Term Fresh -> Term Fresh -> Maybe ()
+addNames :: Term Fresh -> TyM (Term Name)
+addNames = traverse $ \i ->  lift $ asks (M.! i)
+
+typeError :: Term Fresh -> Term Fresh -> TyM a
+typeError l r =
+  (TypeMismatch <$> addNames l <*> addNames r) >>= hlfError . TypeError
+
+typeTerm :: Int -> Context -> Term Fresh -> TyM (Term Fresh)
+typeTerm i cxt t = do
+  namedT <- addNames t
+  local (set termExpr namedT) $
+    case t of
+    Star -> return Star
+    Var j -> lookupVar j cxt
+    Lam body argTy -> typeTerm (i + 1) (bind i argTy cxt) $ unBind i body
+    f :@: a ->
+      typeTerm i cxt f >>= \case
+        Pi ty retTy ->
+          assert [checkTerm i cxt a $ nf ty] (instantiate1 (nf a) retTy)
+        When r l ->
+          assert [checkTerm i cxt a $ nf l] (nf r)
+        ty -> do
+          argTy <- typeTerm i cxt a
+          typeError ty (argTy --> Var (Unbound i))
+    Pi ty body ->
+      assert [ isType i cxt ty
+             , isType (i + 1) (bind i (nf ty) cxt) (unBind i body)]
+      Star
+    When r l -> assert [isType i cxt $ nf l, isType i cxt $ nf r]
+                Star
+
+checkTerm :: Int -> Context -> Term Fresh -> Term Fresh -> ErrorM (Reader NameMap) ()
 checkTerm i cxt term ty = do
   ty' <- typeTerm i cxt term
-  guard (nf ty' == nf ty)
+  when (nf ty' /= nf ty) $ typeError ty ty'
 
-isType :: Int -> Context -> Term Fresh -> Maybe ()
+
+isType :: Int -> Context -> Term Fresh -> TyM ()
 isType i cxt ty = checkTerm i cxt ty Star
 
-typeProgram :: Context -> Bool
-typeProgram cxt = T.all (isJust . typeTerm 0 cxt) cxt
+typeProgram :: NameMap -> Context -> Bool
+typeProgram nm cxt = undefined (T.traverse (typeTerm 0 cxt) cxt)
