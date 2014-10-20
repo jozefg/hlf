77cef025d643f10bb02c4b1217b049923d34795f
Danny Gratzer <danny.gratzer@gmail.com>
2014-10-20 01:16:22 -0400

    Added some nicer error handling for env

--- src/Language/HLF/Env.hs
+++ src/Language/HLF/Env.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Language.HLF.Env where
 import           Control.Applicative
-import qualified Data.Map            as M
+import           Control.Lens         hiding (Context)
+import           Control.Monad.Reader
+import qualified Data.Map             as M
 import           Data.Monoid
-import qualified Data.Text           as T
+import qualified Data.Text            as T
 import           Data.Traversable
 import           Language.HLF.AST
+import           Language.HLF.Error
 
 data Definition a = (:=) { defName :: Name
                          , defTy   :: Term Name }
@@ -16,10 +20,21 @@ newtype Env a = Env {unEnv :: [Definition a]}
               deriving(Monoid)
 type Program = Env Name
 
-bindEnv :: Program -> Maybe Context
-bindEnv (Env env) = foldr bindTy (Just M.empty) env
-  where nameMap = M.fromList $ zip (map defName env) (map Free [0..])
+lookupName :: Monad m => Name -> M.Map Name Fresh -> ErrorM m Fresh
+lookupName name map = case M.lookup name map of
+  Just i -> return i
+  Nothing -> hlfError (EnvError $ UnboundName name)
+
+flipAList :: [(a, b)] -> [(b, a)]
+flipAList = map $ \(a, b) -> (b, a)
+
+bindEnv :: Monad m => Program -> ErrorM m (M.Map Fresh Name, Context)
+bindEnv (Env env) = (M.fromList $ flipAList names,)
+                    <$> foldr bindTy (return M.empty) env
+  where names = zip (map defName env) (map Free [0..])
+        nameMap = M.fromList names
         bindTy (name := ty) tyMap =
-          let name' = nameMap M.! name
-              ty' = traverse (flip M.lookup nameMap) ty
-          in M.insert name' <$> ty' <*> tyMap
+          local (set termName name . set termExpr ty) $
+          M.insert <$> lookupName name nameMap
+                   <*> traverse (flip lookupName nameMap) ty
+                   <*> tyMap
