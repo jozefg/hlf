c299a94ee90e06dfe08640fcecd4f71a3640233f
Danny Gratzer <danny.gratzer@gmail.com>
2014-10-19 23:54:41 -0400

    Tidied up typechecker again

--- src/Language/HLF/TC.hs
+++ src/Language/HLF/TC.hs
@@ -13,32 +13,35 @@ assert as b = T.sequence_ as >> return b
 unBind :: Int -> Scope () Term Fresh -> Term Fresh
 unBind i sc = instantiate1 (Var (Unbound i)) sc
 
+bind :: Int -> Term Fresh -> Context -> Context
+bind = M.insert . Unbound
+
 typeTerm :: Int -> Context -> Term Fresh -> Maybe (Term Fresh)
 typeTerm i cxt t = case t of
   Star -> Just Star
   Var j -> M.lookup j cxt
-  Lam body argTy ->
-    let cxt' = M.insert (Unbound i) argTy cxt
-    in typeTerm (i + 1) cxt' (unBind i body)
+  Lam body argTy -> typeTerm (i + 1) (bind i argTy cxt) $ unBind i body
   f :@: a ->
     case typeTerm i cxt f of
      Just (Pi ty retTy) ->
-       assert [checkTerm i cxt a (nf ty)] (instantiate1 (nf a) retTy)
+       assert [checkTerm i cxt a $ nf ty] (instantiate1 (nf a) retTy)
      Just (When r l) ->
-       assert [checkTerm i cxt a (nf l)] (nf r)
+       assert [checkTerm i cxt a $ nf l] (nf r)
      _ -> Nothing
   Pi ty body ->
-    let val = nf ty
-        cxt' = M.insert (Unbound i) val cxt
-    in assert [ checkTerm i cxt ty Star
-              , checkTerm (i + 1) cxt' (unBind i body) Star] Star
-  When r l -> assert [checkTerm i cxt (nf l) Star, checkTerm i cxt (nf r) Star]
+    assert [ isType i cxt ty
+           , isType (i + 1) (bind i (nf ty) cxt) (unBind i body)]
+    Star
+  When r l -> assert [isType i cxt $ nf l, isType i cxt $ nf r]
               Star
 
 checkTerm :: Int -> Context -> Term Fresh -> Term Fresh -> Maybe ()
 checkTerm i cxt term ty = do
   ty' <- typeTerm i cxt term
   guard (nf ty' == nf ty)
+
+isType :: Int -> Context -> Term Fresh -> Maybe ()
+isType i cxt ty = checkTerm i cxt ty Star
 
 typeProgram :: Context -> Bool
 typeProgram cxt = T.all (isJust . typeTerm 0 cxt) cxt
