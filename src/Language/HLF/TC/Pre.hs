d8c8adb474e3c05c55d0792ee7d5bd33e021bb80
Danny Gratzer <danny.gratzer@gmail.com>
2014-10-23 00:38:38 -0400

    Added pre-tc pass to check for eta long-ness

---
+++ src/Language/HLF/TC/Pre.hs
@@ -0,0 +1,25 @@
+module Language.HLF.TC.Pre where
+import Bound
+import Control.Applicative
+import Control.Lens
+import Control.Monad.Reader
+import Language.HLF.AST
+import Language.HLF.Error
+import Language.HLF.TC.Util
+
+isEtaLong :: Term Fresh -> TyM ()
+isEtaLong = go 0
+  where go i t = do
+          expr <- addNames t
+          local (errorCxt . termExpr .~ Just expr) $
+            case t of
+             Pi l scope ->
+               go i l *> go (i + 1) (instantiate1 (Var $ Unbound i) scope)
+             Lam body argTy ->
+               go i argTy *> go (i + 1) (instantiate1 (Var $ Unbound i) body)
+             When r l -> go i r *> go i l
+             Var _ -> return ()
+             (Var _ :@: a) :@: b -> go i a *> go i b
+             l@(_ :@: _) :@: r -> go i l *> go i r
+             Var _ :@: r -> go i r
+             ap@(_ :@: _) -> etaError ap
