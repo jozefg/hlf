1e81d2b2e85f90e358118df2ddeeecd94df4ce5d
Danny Gratzer <danny.gratzer@gmail.com>
2014-10-23 00:49:42 -0400

    Added note about inconsistent looking typing rule

--- src/Language/HLF/TC/Actual.hs
+++ src/Language/HLF/TC/Actual.hs
@@ -14,7 +14,7 @@ typeTerm i t = do
   namedT <- addNames t
   local (errorCxt . termExpr .~ Just namedT) $
     case t of
-     Star -> return Star
+     Star -> return Star -- See note 1
      Var j -> lookupVar j
      Lam body argTy -> bind i argTy . typeTerm (i + 1) $ unBind i body
      f :@: a -> typeApp i f a
@@ -43,3 +43,10 @@ checkTerm i term ty = do
 
 isType :: Int -> Term Fresh -> TyM ()
 isType i ty = checkTerm i ty Star
+
+-- Note 1. This looks inconsistent but I don't think it actually
+-- is. Since we don't allow Pi types to be indexed over anything other
+-- than *occupants* of Star, we can't get the classic encoding of
+-- Russel's paradox. Really I just need a way to politely say "The
+-- type of this not interesting" instead of lying with this Star :
+-- Star nonsense.
