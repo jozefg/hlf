68227277a88ba036541b743fa2ff7946db689ee1
Danny Gratzer <danny.gratzer@gmail.com>
2014-10-23 23:45:44 -0400

    Long comment/note/apology for tricky code

--- src/Language/HLF/TC/Arity.hs
+++ src/Language/HLF/TC/Arity.hs
@@ -18,7 +18,7 @@ typeArityBound i aMap ty scope = typeArity (i + 1) aMap' term
 
 termArity :: Int -> ArityMap -> Term Fresh -> Int
 termArity i aMap t = case t of
-  Lam scope ty -> termArityBound i aMap ty scope
+  Lam scope ty -> termArityBound i aMap ty scope -- See Note 1
   Var a -> aMap M.! a
   l :@: _ -> termArity i aMap l - 1
   When{} -> 0
@@ -37,3 +37,16 @@ typeArity i aMap t = case t of
 buildArityMap :: Context -> ArityMap
 buildArityMap = foldl' build M.empty . M.toList
   where build aMap (i, term) = M.insert i (typeArity 0 aMap term) aMap
+
+-- Note 1: It's not entirely obvious why we can disregard the addition
+-- of a lambda on a term. The reason is awfully hacky and makes me
+-- feel bad. We're only concerened with beta-normal forms at this
+-- point in typechecking. This means that all types are of the form
+-- `Variable|Constant :@: term :@: term ..` or `Lam ...`. When we're
+-- checking whether a term has an arity of zero, we really don't want
+-- to include lambdas as a +1 even though they're obviously increasing
+-- the arity. Why? Because in this case they're actually being used to
+-- reduce the arity of the term they're closing over. Overall this
+-- leaves me with a bad taste in my mouth even though I'm reasonably
+-- certain I'm correct. I guess this is more apology letter than
+-- comment.
