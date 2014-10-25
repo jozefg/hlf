6bcd81f796814955dd3bc3f47511756a0329fe9a
Danny Gratzer <jozefg@cmu.edu>
2014-10-25 11:38:22 -0400

    Switched Eta to new style DSL

--- examples/Eta.hs
+++ examples/Eta.hs
@@ -3,11 +3,10 @@ module EtaShorthand where
 import Language.HLF
 
 program :: Program
-program = Env [ "Nat" := Star
-              , "z" := "Nat"
-              , "s" := "Nat" --> "Nat"
-              , "bar" := "Nat" --> Star
-              , "test" := "bar":@:("s":@:"z")
-              , "foo" := ("Nat" --> "Nat") --> Star
-              , "good" := "foo" :@: (lam "x" ("s" :@: "x") "Nat")
-              , "bad" := "foo" :@: "s"]
+program = [ TypeFamily ("Nat" := Star)
+            ["z" := "Nat" , "s" := "Nat" --> "Nat"]
+          , TypeFamily ("bar" := "Nat" --> Star)
+            ["test" := "bar":@:("s":@:"z")]
+          , TypeFamily ("foo" := ("Nat" --> "Nat") --> Star)
+            ["good" := "foo" :@: (lam "x" ("s" :@: "x") "Nat")
+            ,"bad" := "foo" :@: "s"]]
