a1024af3f5d1d42a014b52c476d0394a8286a613
Danny Gratzer <jozefg@cmu.edu>
2014-10-25 11:42:52 -0400

    Fixed LC with new style DSL

--- examples/LC.hs
+++ examples/LC.hs
@@ -6,23 +6,23 @@ import Language.HLF
 -- magic. I justify this with the quote "I can, and therefore I
 -- should". At least that's how I think it went...
 program :: Program
-program = Env [ "st"  := Star
-              , "ap"  := "st" --> "st" --> "st"
-              , "lam" := ("st" --> "st") --> "st"
-              , "tt"  := "st"
-              , "ty"  := Star
-              , "arr" := "ty" --> "ty" --> "ty"
-              , "unit" := "ty"
-              , "of" := "st" --> "ty" --> Star
-              , "of/tt" := "of" :@: "tt" :@: "unit"
-              , "of/ap" :=
-                piMany [("st", "f"), ("st", "a"), ("ty", "l"), ("ty", "r")]
-                ("of" :@: ("ap":@:"f":@:"a") :@: "r"
-                 <-- "of" :@: "f" :@: ("arr":@:"l":@:"r")
-                 <-- "of" :@: "a" :@: "l")
-              , "of/lam" :=
-                piMany [("st", "f"), ("ty", "l"), ("ty", "r")]
-                ("of" :@: "f" :@: ("arr":@:"l":@:"r")
-                 <-- (piTy "st" "a" $
-                      "of" :@: "a" :@: "l"
-                      --> "of" :@: ("ap":@:"f":@:"a") :@: "r"))]
+program = [ TypeFamily ("st"  := Star)
+            [ "ap"  := "st" --> "st" --> "st"
+            , "lam" := ("st" --> "st") --> "st"
+            , "tt"  := "st"]
+          , TypeFamily ("ty"  := Star)
+            [ "arr" := "ty" --> "ty" --> "ty"
+            , "unit" := "ty" ]
+          , TypeFamily ("of" := "st" --> "ty" --> Star)
+            [ "of/tt" := "of" :@: "tt" :@: "unit"
+            , "of/ap" :=
+              piMany [("st", "f"), ("st", "a"), ("ty", "l"), ("ty", "r")]
+              ("of" :@: ("ap":@:"f":@:"a") :@: "r"
+               <-- "of" :@: "f" :@: ("arr":@:"l":@:"r")
+               <-- "of" :@: "a" :@: "l")
+            , "of/lam" :=
+              piMany [("st", "f"), ("ty", "l"), ("ty", "r")]
+              ("of" :@: "f" :@: ("arr":@:"l":@:"r")
+               <-- (piTy "st" "a" $
+                    "of" :@: "a" :@: "l"
+                    --> "of" :@: ("ap":@:"f":@:"a") :@: "r"))]]
