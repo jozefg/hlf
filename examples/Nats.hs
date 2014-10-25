91b2c0c1a1f81058dc65f30d69db1c150b113d92
Danny Gratzer <jozefg@cmu.edu>
2014-10-25 11:48:05 -0400

    Update Nats.hs to newstyle and added missing --> Star

--- examples/Nats.hs
+++ examples/Nats.hs
@@ -3,12 +3,12 @@ module Nats where
 import Language.HLF
 
 program :: Program
-program = Env [ "Nat" := Star
-              , "S"   := "Nat" --> "Nat"
-              , "Z"   := "Nat"
-              , "plus" := "Nat" --> "Nat" --> "Nat"
-              , "plusZ" := piTy "Nat" "N" ("plus" :@: "Z" :@: "N" :@: "N")
-              , "plusS" :=
-                piMany [("Nat", "N"), ("Nat", "M"), ("Nat", "O")]
-                ("plus":@:"N":@:"M":@:"O"
-                 <-- "plus":@:("S":@:"N"):@:"M":@:("S":@:"O"))]
+program = [ TypeFamily ("Nat" := Star)
+            [ "S"   := "Nat" --> "Nat"
+            , "Z"   := "Nat"]
+          , TypeFamily ("plus" := "Nat" --> "Nat" --> "Nat" --> Star)
+            [ "plusZ" := piTy "Nat" "N" ("plus" :@: "Z" :@: "N" :@: "N")
+            , "plusS" :=
+              piMany [("Nat", "N"), ("Nat", "M"), ("Nat", "O")]
+              ("plus":@:"N":@:"M":@:"O"
+               <-- "plus":@:("S":@:"N"):@:"M":@:("S":@:"O"))]]
